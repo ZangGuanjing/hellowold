a237026285813145f169b08937cb1f5fac060ccd
ZangGuanjing <zang.gj@vivebest.com>
2020-05-13 10:29:48 +0800

    Initial commit

---
+++ src/main/java/com/zgj/pojoHelloWord.java
@@ -0,0 +1,11 @@
+package com.zgj;
+
+/**
+ * @author Simon
+ * @create 2020-05-13:10:16
+ */
+public class pojoHelloWord {
+    public static void main(String[] args) {
+        System.out.println("212");
+    }
+}
